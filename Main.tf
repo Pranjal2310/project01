52113d2e617dcf95b62bdd8a7b7117b1ec8fd028
Pranjal2310 <100272843+Pranjal2310@users.noreply.github.com>
2023-04-02 16:23:09 +0530

    Create Main.tf

---
+++ Main.tf
@@ -0,0 +1,105 @@
+provider "aws" {
+  region     = "us-east-1"
+  
+}
+resource "aws_vpc" "project" {                # Creating VPC here
+   cidr_block       = "10.0.0.0/16"           ## Defining the CIDR block use 10.0.0.0/24 for demo
+   instance_tenancy = "default"
+
+   tags = {
+    Name = "project"
+  }
+}
+
+
+    
+
+
+resource "aws_subnet" "privatesubnet" {
+   vpc_id =  aws_vpc.project.id
+   cidr_block = "10.0.1.0/24"             # CIDR block of private subnets
+   tags = {
+    Name = "private"
+  }
+ }
+
+
+resource "aws_route_table" "PrivateRT" {    # Creating RT for Private Subnet
+   vpc_id = aws_vpc.project.id
+   route {
+   cidr_block = "0.0.0.0/0"             # Traffic from Private Subnet reaches Internet via NAT Gateway
+   nat_gateway_id = aws_nat_gateway.NATgw.id
+   }
+   tags = {
+    Name = "privaRT"
+  }
+ }
+
+
+
+resource "aws_route_table_association" "PrivateRTassociation" {
+    subnet_id = aws_subnet.privatesubnet.id
+    route_table_id = aws_route_table.PrivateRT.id
+ }
+
+resource "aws_eip" "nateIP" {
+   vpc   = true
+
+  tags = {
+    Name = "EIP"
+  }
+ }
+
+resource "aws_nat_gateway" "NATgw" {
+   allocation_id = aws_eip.nateIP.id
+   subnet_id = aws_subnet.publicsubnet.id
+
+  tags = {
+    Name = "NAT"
+  }
+ }
+
+ # security group
+resource "aws_security_group" "sec_grp" {
+  description = "Allow ssh inbound traffic"
+  vpc_id      = aws_vpc.project.id
+
+  ingress {
+    description      = "ssh from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.project.cidr_block]
+   
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks      = [aws_vpc.project.cidr_block]
+   
+  }
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "sec_grp"
+  }
+}
+
+resource "aws_lambda_function" "lambda"
